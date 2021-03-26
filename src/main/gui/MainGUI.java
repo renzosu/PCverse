df0a42e34bc60ea93e5a0fe61eaf6ac42784f71e
Renzo Su <renzosu@students.cs.ubc.ca>
2021-03-26 11:25:41 -0700

    updated specs

--- src/main/gui/MainGUI.java
+++ src/main/gui/MainGUI.java
@@ -2,7 +2,13 @@ package gui;
 
 import javax.swing.*;
 
+/**
+ * Represents the main window that starts PC Model's internalFrame
+ */
 public class MainGUI {
+    /**
+     * Runs the PC Model
+     */
     public static void main(String[] args) {
         InternalFrame internalFrame = new InternalFrame();
         internalFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
